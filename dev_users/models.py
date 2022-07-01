0b5c8b1ee0dadd504a356d6a9db55f0ed609538f
SaeedG1994 <Bahman.dev19@gmail.com>
2022-07-01 11:38:48 +0430

    Second commit

--- dev_users/models.py
+++ dev_users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 import  uuid
-# Create your models here.
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE,null=True,blank=True,verbose_name='یوزر')
@@ -9,6 +9,7 @@ class Profile(models.Model):
     email = models.EmailField(max_length=500,null=True,blank=True,verbose_name='ایمیل')
     short_bio = models.CharField(max_length=200,null=True,blank=True,verbose_name='معرفی کوتاه')
     bio = models.TextField(null=True,blank=True,verbose_name='بیوگرافی')
+    location = models.CharField(max_length=200,null=True,blank=True,verbose_name='محل زندگی')
     profile_image = models.ImageField(null=True,blank=True,upload_to='profiles/',default="profiles/user-default.png",verbose_name='عکس پروفایل')
     social_github = models.CharField(max_length=200,null=True,blank=True,verbose_name='گیت هاب')
     social_twitter = models.CharField(max_length=200,null=True,blank=True,verbose_name='توئیتر')
@@ -23,3 +24,19 @@ class Profile(models.Model):
     class Meta:
         verbose_name = 'یوزر'
         verbose_name_plural = 'پروفایل'
+
+
+class Skill(models.Model):
+
+    owner = models.ForeignKey(Profile,on_delete=models.CASCADE,null=True,blank=True,verbose_name='یوز مربوط ')
+    name = models.CharField(max_length=200,null=True,blank=True,verbose_name='نام ')
+    description =models.TextField(null=True,blank=True,verbose_name='توضیحات ')
+    created = models.DateTimeField(auto_now_add=True,verbose_name='تاریخ ایجاد ')
+    id = models.UUIDField(default=uuid.uuid4,unique=True,primary_key=True,editable=False)
+
+    def __str__(self):
+        return str(self.name)
+
+    class Meta:
+        verbose_name = 'مهارت'
+        verbose_name_plural = 'مهارت ها'
--- dev_users/views.py
+++ dev_users/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
-
+from .models import Profile
 
 def profiles(request):
-    return render(request, 'Users/profiles.html')
+    profile = Profile.objects.all()
+    context = {
+        'profile':profile,
+
+    }
+    return render(request, 'Users/All-profiles.html', context)
+
+
+def userProfile(request,pk):
+    profile = Profile.objects.get(id=pk)
+    top_skill = profile.skill_set.exclude(description__exact="")
+    other_skill = profile.skill_set.filter(description="")
+    context= {
+        'profile':profile,
+        'top_skill':top_skill,
+        'other_skill':other_skill
+    }
+    return render(request,'Users/user-profile.html',context)
